4f85693fc71de666fd44491eecad252aba05d139
registered <registered>
2019-07-11 10:59:23 +0530

    finitel

---
+++ src/console.java
@@ -0,0 +1,15 @@
+
+public class console {
+	public static void main(String[] args) {
+		String s= "sutha";
+		String r= "";
+		for (int i = s.length()-1; i >=0; i--) {
+			char s2 = s.charAt(i);
+			r=r+s2;
+			
+			
+		}
+		System.out.println(r);
+	}
+
+}
---
+++ src/git.java
@@ -0,0 +1,13 @@
+
+public class git {
+	private static final String HERO="kosaksipazapugazh";
+	public static void main(String[] args) {
+		
+	
+	int[]arr= {9,6,8,1,5,4,2,7,3};
+	int wrap = arr[6];
+	System.out.println((HERO.substring(wrap,HERO.lastIndexOf('z'))));
+
+}
+
+}
