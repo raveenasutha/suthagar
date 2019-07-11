ea5babc8589c2632e9d7d9a665ff30bd7dc86a56
registered <registered>
2019-07-11 11:23:12 +0530

    project

---
+++ src/dear.java
@@ -0,0 +1,14 @@
+
+public class dear {
+	private static final String HERO="kosaksipazapugazh";
+	public static void main(String[] args) {
+		
+	
+	int[]arr= {9,6,8,1,5,4,2,7,3};
+	int wrap = arr[6];
+	System.out.println((HERO.substring(wrap,HERO.lastIndexOf('z'))));
+
+}
+
+
+}
